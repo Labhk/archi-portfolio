82ce68da449529dd8d243e4d0ee3c024229e1a7c
labhk <labh.k2003@gmail.com>
2023-08-27 14:27:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex justify-center items-center">
+        <div className="text-5xl font-bold text-gray-800">
+          Web Portfolio
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
